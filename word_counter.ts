c6ed816c743809b280c007274ba8f9cfa8797f50
Nida Rizwan <143074620+NidzR@users.noreply.github.com>
2024-01-14 16:18:44 +0500

    Typescript CLI_Project Word_Counter

---
+++ word_counter.ts
@@ -0,0 +1,17 @@
+import inquirer from "inquirer";
+
+
+const answers: {
+    Sentence:string
+} = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter your sentence to count the word count: "
+    }
+])
+
+const words = answers.Sentence.trim().split(" ")
+// console.log(words)
+
+console.log(`Your sentence word count is ${words.length}`)
